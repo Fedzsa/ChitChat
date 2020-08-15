4ef6428a69cdc3cb692a965cb47485bfc264e7bf
Ferenc Molnár <fezsa97@gmail.com>
2020-08-15 17:37:37 +0200

    add relationships to the user model

--- models/user.js
+++ models/user.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.hasMany(models.Message);
+      User.belongsToMany(models.Room, { through: models.UsersRooms });
+      User.hasMany(models.Friend, { foreignKey: 'userId', as: 'User' });
+      User.hasMany(models.Friend, { foreignKey: 'friendId', as: 'Friend' });
     }
   };
   User.init({
