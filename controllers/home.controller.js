4cda54756c6a064447a71f88f83a47c117887b55
Ferenc Molnár <fezsa97@gmail.com>
2020-08-17 14:15:00 +0200

    modify friend query and add friend request query

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,9 +1,33 @@
 const { User, Friend } = require('../models/index');
+const { Op } = require("sequelize");
 
 exports.index = async (req, res, next) => {
-    let friends = await req.user.getFriends();
+    let friends = await User.findAll({
+        include: {
+            model: Friend, 
+            as: 'Me',
+            where: {
+                userId: req.user.id,
+                status: 1
+            },
+            attributes: []
+        }
+    });
+
+    let friendRequests = await User.findAll({
+        include: {
+            model: Friend, 
+            as: 'Me', 
+            where: {
+                friendId: req.user.id,
+                status: 0
+            },
+            attributes: []
+        }
+    });
 
     res.render('index', {
-        friends: friends
+        friends: friends,
+        friendRequests: friendRequests
     });
 };
