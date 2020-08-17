cf161c06c1415c2abaeb363e09e2a9c61849b258
Ferenc Molnár <fezsa97@gmail.com>
2020-08-17 10:55:05 +0200

    homecontroller render index page with the user friends

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,4 +1,9 @@
+const { User, Friend } = require('../models/index');
 
 exports.index = async (req, res, next) => {
-    res.render('index');
+    let friends = await req.user.getFriends();
+
+    res.render('index', {
+        friends: friends
+    });
 };
