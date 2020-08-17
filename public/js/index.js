f1c4e3bc0b9544e8218ccc7d092a68c063176b0c
Ferenc Molnár <fezsa97@gmail.com>
2020-08-17 16:34:37 +0200

    add ajax function for accept the friend request

--- public/js/index.js
+++ public/js/index.js
@@ -65,3 +65,23 @@ function addFriend(event) {
     })
     .catch(error => console.log(`Error: ${error}`));
 }
+
+function acceptRequest(event) {
+    let userId = event.target.getAttribute('data-user-id');
+
+    fetch(`/friends/request/accept`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            userId: userId
+        })
+    })
+    .then(response => {
+        if(response.ok) {
+            event.target.parentNode.parentNode.remove();
+        }
+    })
+    .catch(error => console.error(error));
+}
