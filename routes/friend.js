70b90cb2e096697c21719898ef6f0454ecb5484d
Ferenc Molnár <fezsa97@gmail.com>
2020-08-17 16:33:56 +0200

    add accept the friend request route

--- routes/friend.js
+++ routes/friend.js
@@ -4,5 +4,6 @@ const auth = require('../middlewares/auth.middleware');
 const FriendController = require('../controllers/friend.controller');
 
 router.post('/request', auth, FriendController.storeFriendRequest);
+router.post('/request/accept', auth, FriendController.acceptFriendRequest)
 
 module.exports = router;
