09e86b227d15376be5f6833ab45ebee1551614fe
Ferenc Molnár <fezsa97@gmail.com>
2020-08-17 16:36:18 +0200

    create transaction for accept the friend request

--- controllers/friend.controller.js
+++ controllers/friend.controller.js
@@ -1,4 +1,5 @@
 const { Friend } = require('../models/index');
+const sequelize = require('../models/index').sequelize;
 
 exports.storeFriendRequest = async (req, res, next) => {
     await Friend.create({
@@ -11,5 +12,39 @@ exports.storeFriendRequest = async (req, res, next) => {
 };
 
 exports.acceptFriendRequest = async (req, res, next) => {
+    let { userId } = req.body;
+    let transaction = await sequelize.transaction();
 
+    try {
+        await Friend.update({
+            status: 1
+        },
+        {
+            where: {
+                userId: userId,
+                friendId: req.user.id
+            }
+        },
+        {
+            transaction: transaction
+        });
+
+        await Friend.create({
+            userId: req.user.id,
+            friendId: userId,
+            status: 1
+        },
+        {
+            transaction: transaction
+        });
+
+        await transaction.commit();
+    } catch(error) {
+        await transaction.rollback();
+
+        return res.sendStatus(500);
+    }
+
+
+    res.sendStatus(200);
 };
